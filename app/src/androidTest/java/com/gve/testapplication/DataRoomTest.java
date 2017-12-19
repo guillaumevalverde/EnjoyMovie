c14e9f448fa68753673157db4a78c0fcc2147150
valverde Guillaume <guillaume.valverde@gmail.com>
2017-12-19 14:25:26 +0100

    fix integration test for RoomJsonStore

--- app/src/androidTest/java/com/gve/testapplication/DataRoomTest.java
+++ app/src/androidTest/java/com/gve/testapplication/DataRoomTest.java
@@ -9,11 +9,12 @@ import android.support.v4.util.Pair;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import com.gve.testapplication.ListOfRepoFeature.data.ListRepositoryRepo;
-import com.gve.testapplication.ListOfRepoFeature.data.Repository;
 import com.gve.testapplication.core.data.AppDataBase;
 import com.gve.testapplication.core.data.roomjsonstore.RoomJson;
 import com.gve.testapplication.core.data.roomjsonstore.RoomJsonStore;
+import com.gve.testapplication.feature.Movie;
+import com.gve.testapplication.feature.data.ListMovieRepo;
+import com.gve.testapplication.test_common.MovieUtil;
 
 import org.junit.After;
 import org.junit.Before;
@@ -22,9 +23,10 @@ import org.junit.runner.RunWith;
 
 import java.util.List;
 
-import io.reactivex.functions.Predicate;
 import io.reactivex.observers.TestObserver;
 import io.reactivex.subscribers.TestSubscriber;
+
+import static com.gve.testapplication.feature.data.ListMovieRepo.getKeyFromNumPage;
 
 @RunWith(AndroidJUnit4.class)
 public class DataRoomTest {
@@ -49,34 +51,52 @@ public class DataRoomTest {
     @Test
     public void insertSavesData() throws Exception {
         Context appContext = InstrumentationRegistry.getTargetContext();
-        List<Repository> list = PojoUtils.getList(gson).blockingGet();
+        List<Movie> list = MovieUtil.getMoviesSingle(gson).blockingGet();
 
         RoomJson dataToBeSaved = new RoomJson("data1", 10000, gson.toJson(list));
 
         appDataBase.roomJsonModel().add(dataToBeSaved);
 
-        TestObserver<RoomJson> testObserver = appDataBase.roomJsonModel().getItembyIdSingle("data1").test();
+        TestObserver<RoomJson> testObserver =
+                appDataBase.roomJsonModel().getItembyIdSingle("data1").test();
 
         testObserver.assertComplete();
     }
 
     @Test
     public void getInitDataFromStoreIfDataBaseEmpty() throws Exception {
-        RoomJsonStore store = new RoomJsonStore<List<Repository>>(
-                appDataBase,
-                ListRepositoryRepo.getKeyFunction(),
-                json -> gson.fromJson(json, new TypeToken<List<Repository>>(){ }.getType()),
+        RoomJsonStore store = new RoomJsonStore<>(
+                appDataBase.roomJsonModel(),
+                ListMovieRepo.getKeyFunction(),
+                json -> gson.fromJson(json, new TypeToken<List<Movie>>(){ }.getType()),
                 gson::toJson,
                 () -> "[]");
 
-        TestSubscriber<Pair<Long, List<Repository>>> testSubscriber = store.getSingularStream("key").test();
+        TestSubscriber<Pair<Long, List<Movie>>> testSubscriber =
+                store.getSingularStream("key").test();
 
         testSubscriber.assertNoErrors();
-        testSubscriber.assertValueAt(0, new Predicate<Pair<Long, List<Repository>>>() {
-            @Override
-            public boolean test(Pair<Long, List<Repository>> pair) throws Exception {
-                return pair.second.isEmpty();
-            }
-        });
+        testSubscriber.assertValueAt(0, pair -> pair.second.isEmpty());
+    }
+
+    @Test
+    public void dataBaseEmptyThenWeAddSomething() throws Exception {
+        RoomJsonStore<List<Movie>> store = new RoomJsonStore<>(
+                appDataBase.roomJsonModel(),
+                ListMovieRepo.getKeyFunction(),
+                json -> gson.fromJson(json, new TypeToken<List<Movie>>(){ }.getType()),
+                gson::toJson,
+                () -> "[]");
+
+        TestSubscriber<Pair<Long, List<Movie>>> testSubscriber =
+                store.getSingularStream(getKeyFromNumPage(2)).test();
+
+        List<Movie> list = MovieUtil.getMoviesSingle(gson).blockingGet();
+        store.storeSingular(list, 2L);
+
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertValueAt(0, pair -> pair.second.isEmpty());
+
+        testSubscriber.assertValueAt(1, pair -> pair.second.size() == 20);
     }
 }
